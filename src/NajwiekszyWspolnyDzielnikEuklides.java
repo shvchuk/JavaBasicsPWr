9c556e17c6c06b8bf5288fca0f006545e8cb718e
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2016-10-27 13:21:36 +0200

    najwięskzy wspólny dzielnik final ver.

--- src/NajwiekszyWspolnyDzielnikEuklides.java
+++ src/NajwiekszyWspolnyDzielnikEuklides.java
@@ -4,8 +4,19 @@
 public class NajwiekszyWspolnyDzielnikEuklides {
 
     public static void main(String[] args) {
-        
+
+        int m = 58;
+        int n = 36;
+
+        while (m != n ) {
+            if (m < n) n = n - m;
+            else m = m - n;
+        }
+
+        System.out.println("Największy wspólny dzielnik podancyh liczb to: " + m);
+
     }
+
 
 
 }
