1189fcac95e625be23e194fd8a82194d32785090
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2016-10-28 22:42:04 +0200

    kontrolnie

--- src/Kalkulator/Kalkulator.java
+++ src/Kalkulator/Kalkulator.java
@@ -2,9 +2,12 @@ package Kalkulator;
 
 public class Kalkulator {
 
-    public static void main(String[] args) { // metoda główna
+    public static void main(String[] args) { // metoda główna main
 
-        Dodawanie dodajPierwszyZestawLiczb = new Dodawanie(15,20); //
+        // tworzymy zmienną o nazwie dodajPierwszyZestawLiczb typu Dodawanie; zmienna ta zawiera adres do
+        // konkretnego obiektu, który tworzymy poprzez new Dodawanie()
+
+        Dodawanie dodajPierwszyZestawLiczb = new Dodawanie(15,20);
 
         System.out.println("Wynik dodawania: " + dodajPierwszyZestawLiczb.dodawanieDwochLiczb());
     }
