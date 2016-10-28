e228463602fcbddd35d0e44b5abeb04191f99fc1
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2016-10-28 22:06:59 +0200

    dodawanie konstruktor

--- src/Kalkulator/Dodawanie.java
+++ src/Kalkulator/Dodawanie.java
@@ -1,4 +1,17 @@
 package Kalkulator;
 
 public class Dodawanie {
+
+    private int a, b; // zmienne które chcemy do siebie dodać
+
+    public Dodawanie(int a, int b){ // konstruktor - przyjmuje informacje o wartościach dodawanych liczb
+        this.a = a;                 // oraz tworzy operację arytm. pod tyt. dodawanie
+        this.b = b;                 // konstruktor pozwala tworzyć pojedyncze operacje dodawania
+    }
+
+    public int dodawanieDwochLiczb() { // metoda
+        return a + b;
+
+
+    }
 }
--- src/Kalkulator/Kalkulator.java
+++ src/Kalkulator/Kalkulator.java
@@ -1,4 +1,12 @@
 package Kalkulator;
 
 public class Kalkulator {
+
+    public static void main(String[] args) { // metoda główna
+
+        Dodawanie dodajPierwszyZestawLiczb = new Dodawanie(15,20); //
+
+        System.out.println("Wynik dodawania: " + dodajPierwszyZestawLiczb.dodawanieDwochLiczb());
+    }
+
 }
