98a8b0be17d04724ba0538593e8a1f041ccfb479
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2016-10-30 08:52:37 +0100

    liczby pierwsze

--- src/LiczbaPierwsza.java
+++ src/LiczbaPierwsza.java
@@ -1,6 +1,28 @@
 public class LiczbaPierwsza {
 
-    public static void main(String[] args) {
 
+    public static boolean czyLiczbaPierwsza(int liczba) {
+        System.out.println("Sprawdzamy czy liczba " + liczba + " jest pierwsza.");
+
+        boolean liczbaPierwsza = true;
+
+        if (liczba % 2 == 0) { // sprawdzamy czy podana liczba jest parzysta
+            return false;
+        } else {
+            for (int i = 3; i <= liczba / 2; i = i + 2){ // sprawdzamy czy podana liczba dzieli się przez liczbę
+                if (liczba % i == 0){ // nieparzystą 3, 5, 7, 9, 11 itd.
+                    liczbaPierwsza = false;
+                } else {
+                    liczbaPierwsza = true;
+                }
+            }
+        }
+        return liczbaPierwsza;
     }
+
+    public static void main(String[] args) { // metoda główna main
+
+        System.out.println(czyLiczbaPierwsza(151));
+    }
+
 }
